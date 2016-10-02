7f4abef8f752a225c82cb47cf0d32736522000d0
hhoopes <heidih@gmail.com>
2016-10-01 20:17:47 -0600

    Refactor math in #chance method
    While this is a pretty simple refactor to make use of taking in dice
    rolls as an array and to make use of an enumerable, it also belies the
    beginning of decision-making regarding how to handle the mixed use of
    instance and class methods for methods which are only superficially
    different.
    
    While the spec suggests only "minimal" changes to the public interface,
    it also suggests that the interface be consistent and simple, which it
    currently is not. Hence, future commits will reflect decision-making
    regarding whether to keep instance or class methods, and will be single
    commits for each method changed.

--- yatzy.rb
+++ yatzy.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Yatzy
 
   def initialize(d1, d2, d3, d4, d5)
@@ -9,14 +11,8 @@ class Yatzy
     @dice[4] = d5
   end
 
-  def self.chance(d1, d2, d3, d4, d5)
-    total = 0
-    total += d1
-    total += d2
-    total += d3
-    total += d4
-    total += d5
-    return total
+  def self.chance(* rolls)
+    rolls.reduce(:+)
   end
 
   def self.yatzy(d1, d2, d3, d4, d5)
